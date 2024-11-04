52dd2d3b67e65b2df64c567b40870ed3550949f6
Luis Henrique <101938473+Luis-Henrique@users.noreply.github.com>
2024-11-04 19:48:15 -0300

    Update APIFetch.ts

--- src/utils/APIFetch.ts
+++ src/utils/APIFetch.ts
@@ -1,4 +1,4 @@
-const baseURL: string = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
+const baseURL: string = 'www.api.autocare.app.br';
 
 const handleTokenExpiration = () => {
   localStorage.clear();
